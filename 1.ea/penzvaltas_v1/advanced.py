cbb740e8acb11e19e693034ee087f8d78e7de68e
Szabó Gergő <lest12110@gmail.com>
2025-09-08 15:29:50 +0200

    structure, penzvaltas_v2 added

---
+++ 1.ea/penzvaltas_v1/advanced.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+COPYRIGHT NOTICE
+This file was written by Szabó Gergő - https://github.com/szg1
+You are allowed to use it in your own projects
+You are allowed to modify it to suit your needs
+You are required to keep this notice in the file
+You are NOT allowed to publish it as your own work
+You are NOT allowed to use it for commercial purposes
+If you find any bugs, please report them to the author
+"""
+def main() -> None: # főprogram definiálása
+    forint:str = input("Hány forintod van? ") # felhasználói bemenet bekérése
+    forint_szam:int = int(forint) # stringből integer típusúvá alakítás
+    euro:float = forint_szam / 370.0 # átváltás
+    print(f"{forint_szam} forintból {euro:.2f} euró-t tudsz vásárolni") # eredmény kiírása
+
+if __name__ == "__main__": # ha programként futtatod
+    main() # főprogram meghívása
---
+++ 1.ea/penzvaltas_v1/basic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+COPYRIGHT NOTICE
+This file was written by Szabó Gergő - https://github.com/szg1
+You are allowed to use it in your own projects
+You are allowed to modify it to suit your needs
+You are required to keep this notice in the file
+You are NOT allowed to publish it as your own work
+You are NOT allowed to use it for commercial purposes
+If you find any bugs, please report them to the author
+"""
+
+forint=input("Hány forintod van? ")
+euro = int(forint) / 370.0
+print(forint,"forintból",euro,"euró-t tudsz vásárolni")
