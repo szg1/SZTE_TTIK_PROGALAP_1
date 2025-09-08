67dbed6742e9777a42344115b45f94c95bc6e70b
Szabó Gergő <lest12110@gmail.com>
2025-09-08 15:22:23 +0200

    added penzvaltas.py 3 verzioban

--- 1.ea/advanced_penzvaltas.py
+++ 1.ea/advanced_penzvaltas.py
@@ -10,12 +10,12 @@ You are NOT allowed to publish it as your own work
 You are NOT allowed to use it for commercial purposes
 If you find any bugs, please report them to the author
 """
-def main() -> None:
+def main() -> None: # főprogram definiálása
     forint:str = input("Hány forintod van? ") # felhasználói bemenet bekérése
     forint_szam:int = int(forint) # stringből integer típusúvá alakítás
     euro_arfolyam:float = 370.0 # az euró árfolyama
     euro:float = forint_szam / euro_arfolyam # átváltás
     print(f"{forint_szam} forintból {euro:.2f} euró-t tudsz vásárolni") # eredmény kiírása
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": # ha programként futtatod
+    main() # főprogram meghívása
