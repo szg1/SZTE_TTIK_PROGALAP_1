58597a7f2c0eeebc0dc5a383b7cf4040c762524f
Szabó Gergő <lest12110@gmail.com>
2025-09-08 15:19:11 +0200

    added penzvaltas.py 3 verzioban

---
+++ 1.ea/advanced_penzvaltas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+COPYRIGHT NOTICE
+This file was written by Szabó Gergő - https://github.com/szg1
+You are allowed to use it in your own projects
+You are allowed to modify it to suit your needs
+You are required to keep this notice in the file
+You are NOT allowed to publish it as your own work
+You are NOT allowed to use it for commercial purposes
+If you find any bugs, please report them to the author
+"""
+
+forint:str = input("Hány forintod van? ") # felhasználói bemenet bekérése
+forint_szam:int = int(forint) # stringből integer típusúvá alakítás
+euro_arfolyam:float = 370.0 # az euró árfolyama
+euro:float = forint_szam / euro_arfolyam # átváltás
+print(f"{forint_szam} forintból {euro:.2f} euró-t tudsz vásárolni") # eredmény kiírása
